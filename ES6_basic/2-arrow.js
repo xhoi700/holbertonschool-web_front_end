2cef715f8d493d31305671d865f80553f6d0094f
xhoi700 <10057@holbertonstudents.com>
2025-03-26 13:04:44 +0100

    added

---
+++ ES6_basic/2-arrow.js
@@ -0,0 +1,10 @@
+#!/bin/bash
+export default function getNeighborhoodsList() {
+    this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
+  
+    this.addNeighborhood = (newNeighborhood) => {
+      this.sanFranciscoNeighborhoods.push(newNeighborhood);
+      return this.sanFranciscoNeighborhoods;
+    };
+  }
+  
---
+++ ES6_basic/2-main.js
@@ -0,0 +1,6 @@
+#!/bin/bash
+import getNeighborhoodsList from './2-arrow.js';
+
+const neighborhoodsList = new getNeighborhoodsList();
+const res = neighborhoodsList.addNeighborhood('Noe Valley');
+console.log(res);
